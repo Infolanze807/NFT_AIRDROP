854216c2a18da24ff5ebd77538e31478c443deab
infolanze807 <techteam.infolanze@gmail.com>
2024-06-27 12:17:53 +0530

    updatecode

--- src/Airdrop.js
+++ src/Airdrop.js
@@ -291,111 +291,101 @@
 // export default Airdrop
 
 
-import React, {useEffect,useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Web3 from 'web3';
-import { ABI } from './Components/ABI'
+import { ABI } from './Components/ABI';
 
 const Airdrop = () => {
-    const [currentAccount, setCurrentAccount] = useState(null);
-    const [balance, setBalance] = useState(null);
-    const [network, setNetwork] = useState(null);
-    const [nftBalance, setNftBalance] = useState(null);
-    const [tokenIds, setTokenIds] = useState([]);
-    const [transactionHash, setTransactionHash] = useState(null);
-  
+  const [currentAccount, setCurrentAccount] = useState(null);
+  const [balance, setBalance] = useState(null);
+  const [network, setNetwork] = useState(null);
+  const [nftBalance, setNftBalance] = useState(null);
+  const [tokenIds, setTokenIds] = useState([]);
+  const [transactionHash, setTransactionHash] = useState(null);
+
   const ContractAdd = process.env.REACT_APP_CONTRACT_ADDRESS;
   const Private_key = process.env.REACT_APP_PRIVATE_KEY;
   const Owner = process.env.REACT_APP_OWNER_ADDRESS;
-  
-  console.log('Private Key:', Private_key);
-  console.log('Contract Address:', ContractAdd);
-  console.log('Owner Address:', Owner);
-  
-      useEffect(() => {
-      if (window.ethereum) {
-        window.ethereum.on("chainChanged", handleChainChanged);
-        window.ethereum.on("accountsChanged", handleAccountsChanged);
-      }
-  
-      return () => {
-        if (window.ethereum) {
-          window.ethereum.removeListener("chainChanged", handleChainChanged);
-          window.ethereum.removeListener("accountsChanged", handleAccountsChanged);
-        }
-      };
-    }, []);
-  
-   
-const handleChainChanged = async (chainIdHex) => {
-    const networkId = parseInt(chainIdHex, 16); // Convert hexadecimal chainId to decimal
-    console.log("Chain ID returned by MetaMask:", networkId);
 
-    // Handle network change based on networkId
+  useEffect(() => {
+    if (window.ethereum) {
+      window.ethereum.on('chainChanged', handleChainChanged);
+      window.ethereum.on('accountsChanged', handleAccountsChanged);
+    }
+
+    return () => {
+      if (window.ethereum) {
+        window.ethereum.removeListener('chainChanged', handleChainChanged);
+        window.ethereum.removeListener('accountsChanged', handleAccountsChanged);
+      }
+    };
+  }, []);
+
+  const handleChainChanged = async (chainIdHex) => {
+    const networkId = parseInt(chainIdHex, 16); // Convert hexadecimal chainId to decimal
+    console.log('Chain ID returned by MetaMask:', networkId);
+
     switch (networkId) {
       case 713715: // SEI Devnet
-        setNetwork("SEI Devnet");
-        // await updateAccountAndBalance();
+        setNetwork('SEI Devnet');
         break;
       default:
-        setNetwork(null); // Reset network state
-        setCurrentAccount(null); // Reset current account state
-        setBalance(null); // Reset balance state
-        alert("Please switch to SEI Devnet network");
+        setNetwork(null);
+        setCurrentAccount(null);
+        setBalance(null);
+        alert('Please switch to SEI Devnet network');
         await switchToSeiDevnet();
         break;
     }
   };
-  
-    const handleAccountsChanged = async () => {
-      const web3 = new Web3(window.ethereum);
-      const accounts = await web3.eth.getAccounts();
-      if (accounts.length > 0) {
-        setCurrentAccount(accounts[0]);
-        const balanceWei = await web3.eth.getBalance(accounts[0]);
-        const balanceEth = web3.utils.fromWei(balanceWei, "ether");
-        setBalance(balanceEth);
-      } else {
-        setCurrentAccount(null);
-        setBalance(null);
-        setNetwork(null);
-      }
-    };
-  
 
+  const handleAccountsChanged = async () => {
+    const web3 = new Web3(window.ethereum);
+    const accounts = await web3.eth.getAccounts();
+    if (accounts.length > 0) {
+      setCurrentAccount(accounts[0]);
+      const balanceWei = await web3.eth.getBalance(accounts[0]);
+      const balanceEth = web3.utils.fromWei(balanceWei, 'ether');
+      setBalance(balanceEth);
+    } else {
+      setCurrentAccount(null);
+      setBalance(null);
+      setNetwork(null);
+    }
+  };
 
-const connectWallet = async () => {
+  const connectWallet = async () => {
     try {
       if (!window.ethereum.isMetaMask) {
-        return alert("MetaMask is not installed");
+        return alert('MetaMask is not installed');
       }
       const accounts = await window.ethereum.request({
-        method: "eth_requestAccounts",
+        method: 'eth_requestAccounts',
       });
       setCurrentAccount(accounts[0]);
-      console.log("Wallet connected:", accounts[0]);
+      console.log('Wallet connected:', accounts[0]);
 
       const chainIdHex = await window.ethereum.request({
-        method: "eth_chainId",
+        method: 'eth_chainId',
       });
       const networkId = parseInt(chainIdHex, 16); // Convert hexadecimal chainId to decimal
-      console.log("Chain ID returned by MetaMask:", networkId);
+      console.log('Chain ID returned by MetaMask:', networkId);
 
       switch (networkId) {
         case 713715: // SEI Devnet
-          setNetwork("SEI Devnet");
-        //   await updateAccountAndBalance();
+          setNetwork('SEI Devnet');
           break;
         default:
-          setNetwork(null); // Reset network state
-          setCurrentAccount(null); // Reset current account state
-          setBalance(null); // Reset balance state
-          alert("Please switch to SEI Devnet network");
+          setNetwork(null);
+          setCurrentAccount(null);
+          setBalance(null);
+          alert('Please switch to SEI Devnet network');
           await switchToSeiDevnet();
           break;
       }
     } catch (error) {
       console.error(error);
-      alert("An error occurred while connecting to the wallet");
+      alert('An error occurred while connecting to the wallet');
     }
   };
 
@@ -426,200 +416,146 @@ const connectWallet = async () => {
       alert('Failed to switch network.');
     }
   };
-  
-    // const getNetworkName = (networkId) => {
-    //   switch (networkId) {
-    //     case 713715:
-    //       return "SEI DEV"
-    //     default:
-    //       return "Unknown";
-    //   }
-    // };
-  
-    const performNftAirdrop1 = async () => {
-      try {
-        if (!ContractAdd || !currentAccount) {
-          alert("Please enter contract address and recipient address.");
-          return;
-        }
-  
-        const web3 = new Web3(window.ethereum);
-        const contract = new web3.eth.Contract(ABI, ContractAdd);
-  
-        // Fetch token IDs owned by the current user
-        const tokenIds = await contract.methods.tokensOfOwner(Owner).call();
-        if (tokenIds.length === 0) {
-          alert("You do not own any tokens to airdrop.");
-          return;
-        }
-  
-        // const recipientTokenIds = await contract.methods.tokensOfOwner(currentAccount).call();
-        // if (recipientTokenIds.length > 0) {
-        //     alert("Recipient has already received an NFT airdrop.");
-        //     return;
-        // }
-  
-  
-        const gas = await contract.methods
-          .transferFirstTokens(Owner, currentAccount)
-          .estimateGas({ from: Owner });
-  
-        const gasPrice = await web3.eth.getGasPrice();
-  
-        const tx = {
-          from:Owner,
-          to: ContractAdd,
-          gas: gas,
-          gasPrice: gasPrice,
-          data: contract.methods.transferFirstTokens(Owner, currentAccount).encodeABI(),
-        };
-  
-        const signedTx = await web3.eth.accounts.signTransaction(tx, Private_key);
-  
-        const receipt = await web3.eth.sendSignedTransaction(
-          signedTx.rawTransaction
-        );
-  
-        const updatedNftBalance = await contract.methods.balanceOf(Owner).call();
-        setNftBalance(parseInt(updatedNftBalance, 10));
-  
-        const token = await contract.methods.tokensOfOwner(Owner).call();
-        setTokenIds(token.map(id => id.toString()));
-  
-        setTransactionHash(receipt.transactionHash);
-        alert("NFT Airdrop successful!");
-      } catch (error) {
-        console.error("Failed to perform NFT Airdrop:", error);
-        alert("Failed to perform NFT Airdrop.");
+
+  const fetchNftData = async () => {
+    try {
+      if (!Owner || !ContractAdd) {
+        return;
       }
-    };
 
-    const performNftAirdrop2 = async () => {
-        try {
-          if (!ContractAdd || !currentAccount) {
-            alert("Please enter contract address and recipient address.");
-            return;
-          }
-    
-          const web3 = new Web3(window.ethereum);
-          const contract = new web3.eth.Contract(ABI, ContractAdd);
-    
-          // Fetch token IDs owned by the current user
-          const tokenIds = await contract.methods.tokensOfOwner(Owner).call();
-          if (tokenIds.length === 0) {
-            alert("You do not own any tokens to airdrop.");
-            return;
-          }
-    
-        //   const recipientTokenIds = await contract.methods.tokensOfOwner(currentAccount).call();
-        //   if (recipientTokenIds.length > 0) {
-        //       alert("Recipient has already received an NFT airdrop.");
-        //       return;
-        //   }
-    
-    
-          const gas = await contract.methods
-            .transferFirstTwoTokens(Owner, currentAccount)
-            .estimateGas({ from: Owner });
-    
-          const gasPrice = await web3.eth.getGasPrice();
-    
-          const tx = {
-            from:Owner,
-            to: ContractAdd,
-            gas: gas,
-            gasPrice: gasPrice,
-            data: contract.methods.transferFirstTwoTokens(Owner, currentAccount).encodeABI(),
-          };
-    
-          const signedTx = await web3.eth.accounts.signTransaction(tx, Private_key);
-    
-          const receipt = await web3.eth.sendSignedTransaction(
-            signedTx.rawTransaction
-          );
-    
-          const updatedNftBalance = await contract.methods.balanceOf(Owner).call();
-          setNftBalance(parseInt(updatedNftBalance, 10));
-    
-          const token = await contract.methods.tokensOfOwner(Owner).call();
-          setTokenIds(token.map(id => id.toString()));
-    
-          
-          setTransactionHash(receipt.transactionHash);
-          alert("NFT Airdrop successful!");
-        } catch (error) {
-          console.error("Failed to perform NFT Airdrop:", error);
-          alert("Failed to perform NFT Airdrop.");
-        }
-      };
-     
+      const web3 = new Web3(window.ethereum);
+      const contract = new web3.eth.Contract(ABI, ContractAdd);
 
-    useEffect(() => {
-      const fetchNftBalance = async () => {
-        try {
-          if (!Owner || !ContractAdd) {
-            return;
-          }
-  
-          const web3 = new Web3(window.ethereum);
-          const contract = new web3.eth.Contract(ABI, ContractAdd);
-  
-          const balance = await contract.methods.balanceOf(Owner).call();
-          setNftBalance(parseInt(balance, 10));
-        } catch (error) {
-          console.error("Failed to fetch NFT balance:", error);
-          setNftBalance(null);
-        }
+      const balance = await contract.methods.balanceOf(Owner).call();
+      setNftBalance(parseInt(balance, 10));
+
+      const tokenIds = await contract.methods.tokensOfOwner(Owner).call();
+      setTokenIds(tokenIds.map((id) => id.toString()));
+    } catch (error) {
+      console.error('Failed to fetch NFT data:', error);
+      setNftBalance(null);
+      setTokenIds([]);
+    }
+  };
+
+  useEffect(() => {
+    fetchNftData();
+  }, [Owner, ContractAdd]);
+
+  const performNftAirdrop1 = async () => {
+    try {
+      if (!ContractAdd || !currentAccount) {
+        alert('Please enter contract address and recipient address.');
+        return;
+      }
+
+      const web3 = new Web3(window.ethereum);
+      const contract = new web3.eth.Contract(ABI, ContractAdd);
+
+      const tokenIds = await contract.methods.tokensOfOwner(Owner).call();
+      if (tokenIds.length === 0) {
+        alert('You do not own any tokens to airdrop.');
+        return;
+      }
+
+      const gas = await contract.methods
+        .transferFirstTokens(Owner, currentAccount)
+        .estimateGas({ from: Owner });
+
+      const gasPrice = await web3.eth.getGasPrice();
+
+      const tx = {
+        from: Owner,
+        to: ContractAdd,
+        gas: gas,
+        gasPrice: gasPrice,
+        data: contract.methods.transferFirstTokens(Owner, currentAccount).encodeABI(),
       };
-  
-      fetchNftBalance();
-    }, [Owner, ContractAdd]);
-  
-    useEffect(() => {
-      const fetchTokenIds = async () => {
-        try {
-          if (!Owner || !ContractAdd) {
-            return;
-          }
-  
-          const web3 = new Web3(window.ethereum);
-          const contract = new web3.eth.Contract(ABI, ContractAdd);
-          const tokenIds = await contract.methods.tokensOfOwner(Owner).call();
-          setTokenIds(tokenIds.map(id => id.toString()));
-        } catch (error) {
-          console.error("Failed to fetch token IDs:", error);
-          setTokenIds([]);
-        }
+
+      const signedTx = await web3.eth.accounts.signTransaction(tx, Private_key);
+
+      const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+
+      await fetchNftData();
+
+      setTransactionHash(receipt.transactionHash);
+      alert('NFT Airdrop successful!');
+    } catch (error) {
+      console.error('Failed to perform NFT Airdrop:', error);
+      alert('Failed to perform NFT Airdrop.');
+    }
+  };
+
+  const performNftAirdrop2 = async () => {
+    try {
+      if (!ContractAdd || !currentAccount) {
+        alert('Please enter contract address and recipient address.');
+        return;
+      }
+
+      const web3 = new Web3(window.ethereum);
+      const contract = new web3.eth.Contract(ABI, ContractAdd);
+
+      const tokenIds = await contract.methods.tokensOfOwner(Owner).call();
+      if (tokenIds.length === 0) {
+        alert('You do not own any tokens to airdrop.');
+        return;
+      }
+
+      const gas = await contract.methods
+        .transferFirstTwoTokens(Owner, currentAccount)
+        .estimateGas({ from: Owner });
+
+      const gasPrice = await web3.eth.getGasPrice();
+
+      const tx = {
+        from: Owner,
+        to: ContractAdd,
+        gas: gas,
+        gasPrice: gasPrice,
+        data: contract.methods.transferFirstTwoTokens(Owner, currentAccount).encodeABI(),
       };
-  
-      fetchTokenIds();
-    }, [Owner,ContractAdd]);
-  
-  
-  
+
+      const signedTx = await web3.eth.accounts.signTransaction(tx, Private_key);
+
+      const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+
+      await fetchNftData();
+
+      setTransactionHash(receipt.transactionHash);
+      alert('NFT Airdrop successful!');
+    } catch (error) {
+      console.error('Failed to perform NFT Airdrop:', error);
+      alert('Failed to perform NFT Airdrop.');
+    }
+  };
+
   return (
     <div className="box">
-    <h1>NFT Airdrop App</h1>
-    <h3>Remaining NFTs: {nftBalance}</h3>
-    <div className="heder">
-    <div>Connect your wallet</div>
-     <button onClick={connectWallet} className="btn-1">
-       Connect
-      </button>
-</div>
-    <div className="form-main">
-    <div className="form">
-       {ContractAdd && (<p>Contarct Add :{ContractAdd}</p>)}
-       {currentAccount && (<p>Acc :{currentAccount}</p>)}
-       <button onClick={performNftAirdrop1}>Claim 1 NFT</button>
-       <button onClick={performNftAirdrop2}>Claim 2 NFT</button>
-       {transactionHash && (
-         <p>Transaction Hash: <br/>{transactionHash}</p>
-       )}
+      <h1>NFT Airdrop App</h1>
+      <h3>Remaining NFTs: {nftBalance}</h3>
+      <div className="heder">
+        <div>Connect your wallet</div>
+        <button onClick={connectWallet} className="btn-1">
+          Connect
+        </button>
+      </div>
+      <div className="form-main">
+        <div className="form">
+          {ContractAdd && <p>Contract Add: {ContractAdd}</p>}
+          {currentAccount && <p>Acc: {currentAccount}</p>}
+          <button onClick={performNftAirdrop1}>Claim 1 NFT</button>
+          <button onClick={performNftAirdrop2}>Claim 2 NFT</button>
+          {transactionHash && (
+            <p>
+              Transaction Hash: <br />
+              {transactionHash}
+            </p>
+          )}
+        </div>
+      </div>
     </div>
-    </div>
-</div>
-  )
-}
+  );
+};
 
-export default Airdrop
+export default Airdrop;
